0add85af589db8ef2d54454c7ec08a064911474b
=wuhao1994 <=justdowh@126.com>
2017-12-26 15:03:42 +0800

    百度地图api的一些应用

--- boot-mabatis/src/main/resources/static/js/map.js
+++ boot-mabatis/src/main/resources/static/js/map.js
@@ -1,13 +1,9 @@
 // 百度地图API功能
-
-// 百度地图API功能
-// 百度地图API功能
-	function G(id) {
-		return document.getElementById(id);
-	}
-
+    var localpoint ;
+    var endpoint ;
 	var map = new BMap.Map("l-map");
-	map.centerAndZoom("深圳",16);                   // 初始化地图,设置城市和地图级别。
+	var point = new BMap.Point(116.331398,39.897445);
+	map.centerAndZoom(point,16);                   // 初始化地图,设置城市和地图级别。
 	var geolocation = new BMap.Geolocation();
 	//获取当前位置并将地图显示为当前
 	geolocation.getCurrentPosition(function(r){
@@ -15,20 +11,24 @@
 			var mk = new BMap.Marker(r.point);
 			map.addOverlay(mk);
 			map.panTo(r.point);
-			alert('您的位置：'+r.point.lng+','+r.point.lat);
+			localpoint = r.point;
+			console.log(r.point.lng+','+r.point.lat);
 		}
 		else {
 			alert('failed'+this.getStatus());
 		}        
 	},{enableHighAccuracy: true})
-	
 
+	function G(id) {
+		return document.getElementById(id);
+	}
 	var ac = new BMap.Autocomplete(    //建立一个自动完成的对象
 		{"input" : "suggestId"
 		,"location" : map
 	});
 
 	ac.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+		
 	var str = "";
 		var _value = e.fromitem.value;
 		var value = "";
@@ -51,18 +51,21 @@
 	var _value = e.item.value;
 		myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
 		G("searchResultPanel").innerHTML ="onconfirm<br />index = " + e.item.index + "<br />myValue = " + myValue;
-		
 		setPlace();
 	});
 
 	function setPlace(){
-		debugger
 		map.clearOverlays();    //清除地图上所有覆盖物
 		function myFun(){
-			var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
-			map.centerAndZoom(pp, 18);
-			console.log(pp.lng+'--'+pp.lat)
-			map.addOverlay(new BMap.Marker(pp));    //添加标注
+			endpoint = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
+			map.centerAndZoom(endpoint, 18);
+			console.log(endpoint.lng+'--'+endpoint.lat)
+			map.addOverlay(new BMap.Marker(endpoint));    //添加标注
+			var i=$("#driving_way select").val();
+			//DrivingRoute路线      panel 驾车路线文字结果  policy 路线方式（最短 最快 等）
+			var driving = new BMap.DrivingRoute(map, {renderOptions: {map: map, panel: "r-result", autoViewport: true},policy: i});
+			//导航当前地址到目标地址的驾车路线
+			driving.search(localpoint,endpoint);
 		}
 		var local = new BMap.LocalSearch(map, { //智能搜索
 		  onSearchComplete: myFun
@@ -70,3 +73,8 @@
 		local.search(myValue);
 	}
 	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+	$("#result").click(function(){
+		setPlace();
+	});
+	
+//优化方向  1.搜索栏悬浮2.（可以选择起始点，结束点，目前起始点默认本地 选择目标点）	选择目标地址后弹窗公交步行驾车骑行（驾车已实现）3.不同的推荐路线（最快最短不走高速等）4.导航的实景
